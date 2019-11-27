462923a98e21169d35c2bf3c92684653230354e7
Langat <justuslangat78@gmail.com>
2019-11-27 15:04:02 +0300

    added README.md,index.html and js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var add=function(number1, number2){
+return number1+number2;
+};
+alert(add(23,78));
+var multiply=function(joyce, mercy){
+return joyce*mercy;
+};
+alert(multiply(2,9));
+var langat=function(ball1, ball2){
+return ball1/ball2;
+};
+var ball1=parseInt(prompt("enter your number ngombe hii"));
+var ball2=parseInt(prompt("your next number"));
+alert(divide(ball1, ball2));
+
